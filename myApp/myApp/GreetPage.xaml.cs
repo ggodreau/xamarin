04e1ce39cba1664c7e62f02686ab7329f68dbd30
ggodreau <godreau@gmail.com>
2018-05-31 17:13:43 -0500

    1_6 working GreetPage android and iOS

---
+++ myApp/myApp/GreetPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace myApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("myTitle", "myMessage", "OK", "notOK");
+        }
+    }
+}
