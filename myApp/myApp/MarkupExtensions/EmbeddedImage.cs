08f9bf1081af672a497907ce431f4896adae43f3
ggodreau <godreau@gmail.com>
2018-06-11 14:26:19 -0500

    4_40 embedded resource using xaml / markup ext interface (tricky)

---
+++ myApp/myApp/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace myApp.MarkupExtensions
+{
+    // makes it so you don't have to type ResourceId=myApp.images.troll.jpg in XAML
+    [ContentProperty("ResourceId")]
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourceId { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (String.IsNullOrEmpty(ResourceId))
+                return null;
+            return ImageSource.FromResource(ResourceId);
+        }
+    }
+}
