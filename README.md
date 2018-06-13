0e88bbe1d216705bc5aa317eda5584881a498a97
ggodreau <godreau@gmail.com>
2018-06-12 19:08:39 -0500

    4_46 working bacon photo gallery app

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Making sick nasty apps... like dis:
 
-<img src="https://raw.githubusercontent.com/ggodreau/xamarin/master/assets/1.jpg" width="250">    <img src="https://raw.githubusercontent.com/ggodreau/xamarin/master/assets/2.jpg" width="250">    <img src="https://raw.githubusercontent.com/ggodreau/xamarin/master/assets/3.jpg" width="250"> 
+<img src="https://raw.githubusercontent.com/ggodreau/xamarin/master/assets/1.jpg" width="250">    <img src="https://raw.githubusercontent.com/ggodreau/xamarin/master/assets/2.jpg" width="250">    <img src="https://raw.githubusercontent.com/ggodreau/xamarin/master/assets/4.jpg" width="250">
+<img src="https://raw.githubusercontent.com/ggodreau/xamarin/master/assets/3.jpg" width="250"> 
--- myApp/myApp/ImagePage.xaml.cs
+++ myApp/myApp/ImagePage.xaml.cs
@@ -14,9 +14,33 @@ namespace myApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class ImagePage : ContentPage
 	{
+
+        //string[] imgList = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        int curPhoto = 0;
+        string baseUrl = "http://placebacon.net/1920/1080?image=";
+        
 		public ImagePage ()
 		{
 			InitializeComponent ();
+            btnLeft.Image = (FileImageSource) ImageSource.FromFile("arrow_left.png");
+            btnRight.Image = (FileImageSource) ImageSource.FromFile("arrow_right.png");
 		}
-	}
+
+        private void btnLeft_Clicked(object sender, EventArgs e)
+        {
+            curPhoto -= 1;
+            if (curPhoto < 0)
+                curPhoto = 9;
+            var source = new Uri(baseUrl + curPhoto.ToString());
+            mainPhoto.Source = UriImageSource.FromUri(source);
+        }
+
+        private void btnRight_Clicked(object sender, EventArgs e)
+        {
+            curPhoto += 1;
+            if (curPhoto >= 9)
+                curPhoto = 0;
+            var source = new Uri(baseUrl + curPhoto.ToString());
+            mainPhoto.Source = UriImageSource.FromUri(source);
+    }
 }
