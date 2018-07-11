cd84fce3080ec98224b6945eb94b394595c38687
ggodreau <godreau@gmail.com>
2018-07-11 15:37:59 -0500

    5_49 listview with images

--- myApp/myApp/MainPage.xaml.cs
+++ myApp/myApp/MainPage.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using myApp.models;
 
 namespace myApp
 {
@@ -13,15 +14,12 @@ namespace myApp
 		{
 			InitializeComponent();
 
-            var names = new List<string>
+            myList.ItemsSource = new List<Contact>
             {
-                "Greg",
-                "Mosh",
-                "Joyshallow"
+                new Contact(){ ImageUrl = "https://lh5.googleusercontent.com/-8NBn7h8s7eQ/AAAAAAAAAAI/AAAAAAAAAAs/_F1iLsrgmm0/s92-c-k-no/photo.jpg", Name="Sue", Status="Hot" },
+                new Contact(){ ImageUrl = "https://lh3.googleusercontent.com/-c5cBgaRl8Og/AAAAAAAAAAI/AAAAAAAAI04/nEPGtW2ofUs/s56-c-k-no/photo.jpg", Name="Greg", Status="Hotter" },
+                new Contact(){ ImageUrl = "https://scontent-dfw5-2.cdninstagram.com/vp/822a902554acd56ed68f92878fc42919/5BC5EE14/t51.2885-19/s320x320/29400710_162833257867970_5332315182041399296_n.jpg", Name="Leo", Status="Hottest" }
             };
-
-            myList.ItemsSource = names;
-
 		}
 	}
 }
