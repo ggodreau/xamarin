3daef8ec627389ba401d7b6bfeaa25037ab69abc
ggodreau <godreau@gmail.com>
2018-07-17 14:10:00 -0500

    5_51 list grouping

--- myApp/myApp/MainPage.xaml.cs
+++ myApp/myApp/MainPage.xaml.cs
@@ -14,11 +14,20 @@ namespace myApp
 		{
 			InitializeComponent();
 
-            myList.ItemsSource = new List<Contact>
+            myList.ItemsSource = new List<ContactGroup>
             {
-                new Contact(){ ImageUrl = "https://lh5.googleusercontent.com/-8NBn7h8s7eQ/AAAAAAAAAAI/AAAAAAAAAAs/_F1iLsrgmm0/s92-c-k-no/photo.jpg", Name="Sue", Status="Hot" },
-                new Contact(){ ImageUrl = "https://lh3.googleusercontent.com/-c5cBgaRl8Og/AAAAAAAAAAI/AAAAAAAAI04/nEPGtW2ofUs/s56-c-k-no/photo.jpg", Name="Greg", Status="Hotter" },
-                new Contact(){ ImageUrl = "https://scontent-dfw5-2.cdninstagram.com/vp/822a902554acd56ed68f92878fc42919/5BC5EE14/t51.2885-19/s320x320/29400710_162833257867970_5332315182041399296_n.jpg", Name="Leo", Status="On Fire" }
+                new ContactGroup("Sorta Hot", "M")
+                {
+                    new Contact(){ ImageUrl = "https://lh5.googleusercontent.com/-8NBn7h8s7eQ/AAAAAAAAAAI/AAAAAAAAAAs/_F1iLsrgmm0/s92-c-k-no/photo.jpg",
+                        Name ="Sue", Status="Hot" },
+                    new Contact(){ ImageUrl = "https://lh3.googleusercontent.com/-c5cBgaRl8Og/AAAAAAAAAAI/AAAAAAAAI04/nEPGtW2ofUs/s56-c-k-no/photo.jpg",
+                        Name ="Greg", Status="Hotter" }
+                },
+                new ContactGroup("Crazy Hot", "J")
+                {
+                    new Contact(){ ImageUrl = "https://scontent-dfw5-2.cdninstagram.com/vp/822a902554acd56ed68f92878fc42919/5BC5EE14/t51.2885-19/s320x320/29400710_162833257867970_5332315182041399296_n.jpg",
+                        Name ="Leo", Status="On Fire" }
+                }
             };
 		}
 	}
