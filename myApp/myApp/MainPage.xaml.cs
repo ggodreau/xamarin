d4f5f2eb27e566d4350b629232ed517390419f3e
ggodreau <godreau@gmail.com>
2018-07-23 14:28:51 -0500

    5_53 context actions ObservableCollection<> delete from list

--- myApp/myApp/MainPage.xaml.cs
+++ myApp/myApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +11,23 @@ namespace myApp
 {
 	public partial class MainPage : ContentPage
 	{
+        private ObservableCollection<Contact> _contacts;
+
 		public MainPage()
 		{
 			InitializeComponent();
 
-            myList.ItemsSource = new List<ContactGroup>
+            _contacts = new ObservableCollection<Contact>()
             {
-                new ContactGroup("Sorta Hot", "M")
-                {
-                    new Contact(){ ImageUrl = "https://lh5.googleusercontent.com/-8NBn7h8s7eQ/AAAAAAAAAAI/AAAAAAAAAAs/_F1iLsrgmm0/s92-c-k-no/photo.jpg",
-                        Name ="Sue", Status="Hot" },
-                    new Contact(){ ImageUrl = "https://lh3.googleusercontent.com/-c5cBgaRl8Og/AAAAAAAAAAI/AAAAAAAAI04/nEPGtW2ofUs/s56-c-k-no/photo.jpg",
-                        Name ="Greg", Status="Hotter" }
-                },
-                new ContactGroup("Crazy Hot", "J")
-                {
-                    new Contact(){ ImageUrl = "https://scontent-dfw5-2.cdninstagram.com/vp/822a902554acd56ed68f92878fc42919/5BC5EE14/t51.2885-19/s320x320/29400710_162833257867970_5332315182041399296_n.jpg",
-                        Name ="Leo", Status="On Fire" }
-                }
+                new Contact(){ ImageUrl = "https://lh5.googleusercontent.com/-8NBn7h8s7eQ/AAAAAAAAAAI/AAAAAAAAAAs/_F1iLsrgmm0/s92-c-k-no/photo.jpg",
+                    Name ="Sue", Status="Hot" },
+                new Contact(){ ImageUrl = "https://lh3.googleusercontent.com/-c5cBgaRl8Og/AAAAAAAAAAI/AAAAAAAAI04/nEPGtW2ofUs/s56-c-k-no/photo.jpg",
+                    Name ="Greg", Status="Hotter" },
+                new Contact(){ ImageUrl = "https://scontent-dfw5-2.cdninstagram.com/vp/822a902554acd56ed68f92878fc42919/5BC5EE14/t51.2885-19/s320x320/29400710_162833257867970_5332315182041399296_n.jpg",
+                    Name ="Leo", Status="On Fire" }
             };
+
+            myList.ItemsSource = _contacts;
 		}
 
         private void myList_ItemTapped(object sender, ItemTappedEventArgs e)
@@ -52,11 +51,13 @@ namespace myApp
         private void Call_Clicked(object sender, EventArgs e)
         {
             var menuItem = sender as MenuItem;
-            DisplayAlert("Call Item", foo.CommandParameter.ToString(), "Yep");
+            DisplayAlert("Call Item", e.GetType().ToString(), "Yep");
         }
         private void Delete_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Call Item", sender.GetType().ToString(), "Yep");
+            var contact = (sender as MenuItem).CommandParameter as Contact;
+            DisplayAlert("Call Item", contact.Name, "Yep");
+            _contacts.Remove(contact);
         }
     }
 }
