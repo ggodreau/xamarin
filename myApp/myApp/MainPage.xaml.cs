beae6cd9e9b4e469e6f164e8a3abb244717baedb
ggodreau <godreau@gmail.com>
2018-08-02 17:36:10 -0500

    7_65 DisplayActionSheet() multiple button async await prompt

--- myApp/myApp/MainPage.xaml.cs
+++ myApp/myApp/MainPage.xaml.cs
@@ -7,11 +7,18 @@ using Xamarin.Forms;
 
 namespace myApp
 {
-	public partial class MainPage : CarouselPage
+	public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+        async private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var myList = new string[] { "el1", "el2" };
+            string x = await DisplayActionSheet("title", "cancel", "destruct", myList );
+            await DisplayAlert("foo", x, "cancel");
+        }
+    }
 }
