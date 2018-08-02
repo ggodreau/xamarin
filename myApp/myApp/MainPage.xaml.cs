03e19600239ecffa262221d4a67c85b560a15f03
ggodreau <godreau@gmail.com>
2018-08-02 16:16:37 -0500

    7_64 carousel pages

--- myApp/myApp/MainPage.xaml.cs
+++ myApp/myApp/MainPage.xaml.cs
@@ -4,27 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace myApp
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : TabbedPage 
+	public partial class MainPage : CarouselPage
 	{
-        INavigation navigation;
-
 		public MainPage()
 		{
 			InitializeComponent();
-            this.BarTextColor = Color.LawnGreen;
-            this.BarBackgroundColor = Color.Honeydew;
-            navigation = this.Navigation;
-            this.Padding = new Thickness(20, 20, 20, 20); 
 		}
-
-        async private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
-        {
-            await DisplayAlert("oh yea", navigation.ToString(), "ok");
-        }
-    }
+	}
 }
